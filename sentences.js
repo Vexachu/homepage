f195cfe6f368738cf0fa4f4ca803e90c4622f460
Vexachu <pitchblackvorona@gmail.com>
2025-09-21 01:38:13 -0500

    Create sentences.js

---
+++ sentences.js
@@ -0,0 +1,13 @@
+// sentences.js
+// Beginner sentence bank for typing trainer
+
+const SENTENCES = [
+  "The cat sat on the mat.",
+  "I like to run fast.",
+  "We can go to the park.",
+  "She has a red dress.",
+  "He is a good boy.",
+  "It is a sunny day.",
+  // … include all 500+ sentences exactly as you pasted …
+  "We are having fun."
+];
